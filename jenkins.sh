681efaf5220b9b449dcff92622ab1a6068c6c064
Benedikt Kristinsson <benedikt@lokun.is>
2020-12-20 02:06:15 +0100

    forgot to remove sudo

--- jenkins.sh
+++ jenkins.sh
@@ -8,21 +8,21 @@ build_debian() {
     cp emacs-${VERSION}.tar.xz debian/
     PREFIX=/emacs/target
 
-    sudo docker build --build-arg PREFIX=$PREFIX --build-arg VERSION=$VERSION -t emacs-debian debian/
+    docker build --build-arg PREFIX=$PREFIX --build-arg VERSION=$VERSION -t emacs-debian debian/
 
     # debian tags:
     #  * :debian
     #  * :27.1
     #  * :27.1-debian
 
-    sudo docker tag emacs-debian benediktkr/emacs:debian
-    sudo docker tag emacs-debian benediktkr/emacs:${VERSION}
-    sudo docker tag emacs-debian benediktkr/emacs:${VERSION}-debian
+    docker tag emacs-debian benediktkr/emacs:debian
+    docker tag emacs-debian benediktkr/emacs:${VERSION}
+    docker tag emacs-debian benediktkr/emacs:${VERSION}-debian
 
-    sudo docker rm emacs-debian-package || true
-    sudo docker run --name emacs-debian-package emacs-debian
-    sudo docker cp emacs-debian-package:/emacs/debian dist/
-    sudo docker rm emacs-debian-package
+    docker rm emacs-debian-package || true
+    docker run --name emacs-debian-package emacs-debian
+    docker cp emacs-debian-package:/emacs/debian dist/
+    docker rm emacs-debian-package
 
 
 }
@@ -33,19 +33,19 @@ build_amzn () {
 
     PREFIX=/home/bkristinsson/.local
 
-    sudo docker build --build-arg PREFIX=$PREFIX --build-arg VERSION=$VERSION -t emacs-amzn amzn/
+    docker build --build-arg PREFIX=$PREFIX --build-arg VERSION=$VERSION -t emacs-amzn amzn/
 
     # amzn tags:
     #  * :amzn
     #  * :27.1-amzn
 
-    sudo docker tag emacs-amzn benediktkr/emacs:amzn
-    sudo docker tag emacs-amzn benediktkr/emacs:${VERSION}-amzn
+    docker tag emacs-amzn benediktkr/emacs:amzn
+    docker tag emacs-amzn benediktkr/emacs:${VERSION}-amzn
 
-    sudo docker rm emacs-amzn-package || true
-    sudo docker run --name emacs-amzn-package emacs-amzn
-    sudo docker cp emacs-amzn-package:/emacs/amzn dist/
-    sudo docker rm emacs-amzn-package
+    docker rm emacs-amzn-package || true
+    docker run --name emacs-amzn-package emacs-amzn
+    docker cp emacs-amzn-package:/emacs/amzn dist/
+    docker rm emacs-amzn-package
 
 }
 
